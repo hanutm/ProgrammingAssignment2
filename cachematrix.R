fe45e6e7088905c5d63b21f1c443ce2451825b70
Hanut Mathur <hanut.mathur@live.in>
2017-04-03 00:41:28 +0530

    Commit 2- removed function1

--- cachematrix.R
+++ cachematrix.R
@@ -1,22 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()){
-			i <<- NULL                              # i carries the cached inverse of the matrix 
-			set <- function(b) {                    # setter function for changing the matrix
-				a <<- b
-				i <<- NULL
-			}
-			get <- function() a                     # getter function for reading value of current matrix
-			setinv <- function(inv) i <<- inv       # setter function for setting the output of function (here inverse)
-			getinv <- function() i                  # function for getting the output of the function
-			list(set = set,                         
-			     get = get,
-			     setinverse = setinv,
-			     getinverse = getinv)
-}
 
 cacheSolve <- function(a, ...) {                                #function for actual computation
 			i <- a$getinverse()                     
